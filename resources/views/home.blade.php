000470b3986b4683eab7d3fdba824917d6cf5f43
rluciano1965 <rluciano@pbh.gov.br>
2022-03-16 22:35:47 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Bem vindo ao ambiente de suporte em campo SUFIS/GINFI</h1>
+@stop
+
+@section('content')
+    <p>Este ambiente tem como foco o suporte da fiscalização na ponta</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script></script>
+@stop
